2a0aea47ed69833af58c2e94d3d712bf06bb8adc
Brett Maroni-Rana <74420417+b-maroni-rana@users.noreply.github.com>
2020-11-13 10:29:15 -0800

    Delete feature_table.sh

--- feature_table.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env/ bash
-# feature_table.sh
-# creates feature table that includes multiple statistics
-
-qiime metadata tabulate \
-  --m-input-file stats-dada2.qza \
-  --o-visualization stats-dada2.qzv
