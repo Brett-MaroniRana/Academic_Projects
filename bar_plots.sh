dcb67040b68234f3affa4ee490ca94670d737f13
Brett Maroni-Rana <74420417+b-maroni-rana@users.noreply.github.com>
2020-11-13 10:28:22 -0800

    Delete bar_plots.sh

--- bar_plots.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# bar_plots.sh
-
-qiime taxa barplot \
-  --i-table table.qza \
-  --i-taxonomy taxonomy.qza \
-  --m-metadata-file sample-metadata.tsv \
-  --o-visualization taxa-bar-plots.qzv
