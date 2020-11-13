fd3951f1becda76fffc89e818fa3eff41132e849
Brett Maroni-Rana <74420417+b-maroni-rana@users.noreply.github.com>
2020-11-13 10:28:15 -0800

    Delete ancom.sh

--- ancom.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# ancom.sh
-
-qiime composition ancom \
-  --i-table comp-gut-table.qza \
-  --m-metadata-file sample-metadata.tsv \
-  --m-metadata-column subject \
-  --o-visualization ancom-subject.qzv
